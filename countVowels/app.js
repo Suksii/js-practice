cbf7a149f586a5a7f523753b1bcdc426d31cb4fa
unknown <ramovic225@gmail.com>
2025-02-09 18:28:07 +0100

    countVowels#2

--- countVowels/app.js
+++ countVowels/app.js
@@ -1,15 +1,24 @@
+// const countVowels = (str) => {
+//   const vowels = "aeiou";
+//   const vowelsArray = vowels.split("");
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = 0; j < vowelsArray.length; j++) {
+//       if (str[i].toLowerCase() === vowelsArray[j]) {
+//         count++;
+//       }
+//     }
+//   }
+//   return count;
+// };
+
 const countVowels = (str) => {
   const vowels = "aeiou";
-  const vowelsArray = vowels.split("");
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < vowelsArray.length; j++) {
-      if (str[i].toLowerCase() === vowelsArray[j]) {
-        count++;
-      }
-    }
-  }
-  return count;
+  const substr = str
+    .toLowerCase()
+    .split("")
+    .filter((s) => vowels.includes(s));
+  return substr.length;
 };
 
-console.log(countVowels("Iiii"));
+console.log(countVowels("Hello Kitty"));
