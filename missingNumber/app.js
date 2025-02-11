ba905b08298edff48f42d617ce644a3ae671fe4e
unknown <ramovic225@gmail.com>
2025-02-11 19:31:35 +0100

    missingNumber#2

--- missingNumber/app.js
+++ missingNumber/app.js
@@ -1,14 +1,22 @@
 const missingNumber = (arr) => {
   const numArray = [];
 
-  for (let i = 1; i < arr.length + 1; i++) {
-    numArray.push(i);
-  }
-  let br = 0;
-  for (let i = 0; i < numArray.length; i++) {
-    if (numArray[i] !== arr[i]) return numArray[i];
-  }
-  return "There is no missing number";
-};
+  //   for (let i = 1; i < arr.length + 1; i++) {
+  //     numArray.push(i);
+  //   }
+  //   for (let i = 0; i < numArray.length; i++) {
+  //     if (numArray[i] !== arr[i]) {
+  //       return numArray[i];
+  //     }
+  //   }
+  //   return "There is no missing number";
 
-console.log(missingNumber([1, 3, 4, 5, 6, 7, 8]));
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = arr.reduce((prev, curr) => prev + curr, 0);
+
+  const missingNumber = expectedSum - actualSum;
+
+  return missingNumber;
+};
+console.log(missingNumber([1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13]));
