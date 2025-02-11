60b50908f5ca061344308a61c90dc7e92ce1a0ae
unknown <ramovic225@gmail.com>
2025-02-11 16:37:47 +0100

    isArmstrong#2

--- isArmstrong/app.js
+++ isArmstrong/app.js
@@ -1,13 +1,18 @@
 const isArmstrong = (num) => {
   const numArray = num.toString().split("").map(Number);
-  return (
-    numArray.reduce(
-      (prev, curr) => prev + Math.pow(curr, numArray.length),
-      0
-    ) === num
-  );
+  //   return (
+  //     numArray.reduce(
+  //       (prev, curr) => prev + Math.pow(curr, numArray.length),
+  //       0
+  //     ) === num
+  //   );
+  let sum = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    sum += Math.pow(numArray[i], numArray.length);
+  }
+  return sum === num;
 };
 
 console.log(isArmstrong(153));
-console.log(isArmstrong(407));
+console.log(isArmstrong(9474));
 console.log(isArmstrong(370));
